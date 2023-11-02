71461b167c75c5eec2f5bc2b5a6274886e82a4fd
Peter McEvoy <git@mcevoypeter.com>
2023-11-02 14:09:26 -0400

    Create IAM role

--- main.tf
+++ main.tf
@@ -15,3 +15,40 @@ import {
   to = aws_iam_openid_connect_provider.this
   id = var.gh_idp_arn
 }
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${var.account_id}:oidc-provider/${var.gh_idp}"]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringEquals"
+      variable = "${var.gh_idp}:aud"
+      values   = [var.gh_idp_aud]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${var.gh_idp}:sub"
+      values = [
+        for branch in var.branches :
+        format("repo:${var.owner}/${var.repo}:ref:refs/heads/%s", branch)
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name                = "GitHubActions-${var.owner}-${var.repo}-${join("_", var.branches)}"
+  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = var.policy_arns
+  dynamic "inline_policy" {
+    for_each = var.inline_policies
+    content {
+      name   = inline_policy.value["name"]
+      policy = inline_policy.value["policy"]
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -4,7 +4,52 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "account_id" {
+  description = "AWS account ID"
+  type        = number
+}
+
 variable "gh_idp_arn" {
   description = "GitHub OpenID Connect (OIDC) provider ARN"
   type        = string
+}
+
+variable "gh_idp" {
+  description = "GitHub OIDC provider name"
+  type        = string
+  default     = "token.actions.githubusercontent.com"
+}
+
+variable "gh_idp_aud" {
+  description = "GitHub OIDC provider audience"
+  type        = string
+  default     = "sts.amazonaws.com"
+}
+
+variable "owner" {
+  description = "GitHub user that owns var.repo"
+  type        = string
+}
+
+variable "repo" {
+  description = "GitHub repository the GitHub Actions workflows are running in"
+  type        = string
+}
+
+variable "branches" {
+  description = "Git branches that can access resources in var.account_id"
+  type        = set(string)
+}
+
+variable "policy_arns" {
+  description = "ARNs of permissions policies to grant to GitHub Actions workflows"
+  type        = set(string)
+  default     = []
+}
+
+variable "inline_policies" {
+  description = "Inline policies to grant to GitHub Actions workflows"
+  # See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role#inline_policy.
+  type    = set(object({ name = string, policy = string }))
+  default = []
 }
