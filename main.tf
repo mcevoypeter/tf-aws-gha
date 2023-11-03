4109369a17f364250cbcf536197c711bc93c3479
Peter McEvoy <git@mcevoypeter.com>
2023-11-03 14:29:12 -0400

    Remove AWS provider
    Providers should not be defined within modules per
    https://developer.hashicorp.com/terraform/language/modules/develop/providers.

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "account_id" {
   description = "AWS account ID"
   type        = number
